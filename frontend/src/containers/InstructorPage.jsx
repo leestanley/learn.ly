f3c7c1a32b5e9c55e244158b7df0ee9ba6e8e235
eminguyen <emilyhuongnguyen@gmail.com>
2020-02-01 00:34:29 -0800

    Set up redux boilerplate

---
+++ frontend/src/containers/InstructorPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InstructorPage = () => {
+  return (
+    <h1>instructor</h1>
+  );
+};
+
+export default InstructorPage;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.less';
-import App from './App';
+import { ConnectedRouter } from 'connected-react-router';
+import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+// TODO: Set up Redux???
+import configureStore, { history } from './store';
+
+import HomePage from './containers/HomePage';
+import InstructorPage from './containers/InstructorPage';
+
+const store = configureStore();
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <>
+          <Switch>
+            <Route exact path="/instructor" component={InstructorPage} />
+            <Route path="/" component={HomePage} />
+          </Switch>
+        </>
+      </ConnectedRouter>
+    </Provider>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
