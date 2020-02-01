697a67e2fa2349f41c4dea3c13946dcc8cecc658
Kendall Nakai <kendallnakai@gmail.com>
2020-02-01 14:05:25 -0800

    Added LBeginForm and LearnerBegin components
    Missing correct formatting for LearnerBegin page

---
+++ frontend/src/components/LBeginForm/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Button from '../Button';
+import { Form, Input, Radio } from 'antd';
+import { NavLink } from 'react-router-dom';
+
+import './style.less';
+
+const LBeginForm = (props) => {
+  return (
+    <div className="LBeginForm">
+        <h3> View a cast </h3>
+        <Col span={8} /> Something </h3>
+         <Form.Item>
+           <div>
+             <Radio.Group className="radio"
+               onChange={language=> props.setFieldValue('language', language.target.value)}
+               defaultValue="English">
+               <Radio.Button className="radio-button" value="English">English</Radio.Button>
+               <Radio.Button className="radio-button" value="Italian">Italian</Radio.Button>
+               <Radio.Button className="radio-button" value="Chinese">Chinese</Radio.Button>
+               <Radio.Button className="radio-button" value="German">German</Radio.Button>
+             </Radio.Group>
+           </div>
+          </Form.Item>
+            <Button className="button" color='#B6CBFE' size='20px' width='120px' htmlType="submit">
+              Submit
+            </Button>
+      </form>
+  </div>
+);
+};
+
+export default LBeginForm;
