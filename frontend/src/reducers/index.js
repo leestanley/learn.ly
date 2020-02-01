5971e87d8b442ae0cc011372e0b0cbce25c47287
eminguyen <emilyhuongnguyen@gmail.com>
2020-02-01 07:43:52 -0800

    Set up action for ibegin form

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 
+import StreamReducer from './StreamReducer';
+
 export default history =>
   combineReducers({
     router: connectRouter(history),
+    stream: StreamReducer
   });
