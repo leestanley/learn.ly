01a993a847362f238dd614dd990dd9b72ace6984
leestanley <stanleylee91776@gmail.com>
2020-02-03 13:28:56 -0800

    omo

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -1,13 +1,14 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBcz83ZxET2IbHJ7SjT3juiChYpJyW21_M",
-  authDomain: "acmhacksc.firebaseapp.com",
-  databaseURL: "https://acmhacksc.firebaseio.com",
-  projectId: "acmhacksc",
-  storageBucket: "acmhacksc.appspot.com",
-  messagingSenderId: "822135956634",
-  appId: "1:822135956634:web:2f1729e8cb2b3d8224ab23"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
